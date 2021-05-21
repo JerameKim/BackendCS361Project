38af1566bb4f27afe65be0af62553c2d43a19bb7
JerameKim <jerame.y.kim@gmail.com>
2021-05-21 12:35:12 -0700

    No data possibility caught

--- main.py
+++ main.py
@@ -83,20 +83,24 @@ def get_abstract(lang: str, tag: str):
     # format the text in lxml format 
     soup = BeautifulSoup(html_text, "lxml")
 
-    # gets all text content from page
-    content = soup.find('div', class_ ='mw-parser-output')
+    try: 
+        # gets all text content from page
+        content = soup.find('div', class_ ='mw-parser-output')
 
-    # gets all top level 'p' tags, which holds all the text
-    paragraphs = content.find_all('p', recursive=False)
-    
+        # gets all top level 'p' tags, which holds all the text
+        paragraphs = content.find_all('p', recursive=False)
+        
 
-    for i in range(3): 
-        # get the first paragraph tag that has content, this is usually the abstract
-        if(len(paragraphs[i]) > 1): 
-            firstParagraph = paragraphs[i].get_text()
-            break
+        for i in range(3): 
+            # get the first paragraph tag that has content, this is usually the abstract
+            if(len(paragraphs[i]) > 1): 
+                firstParagraph = paragraphs[i].get_text()
+                break
 
-    return firstParagraph
+        return firstParagraph
+
+    except: 
+        return ""
 
 @app.get('/text/{lang}/{tag}')
 def get_main_text(lang: str, tag: str): 
@@ -126,34 +130,31 @@ def get_main_text(lang: str, tag: str):
     # format the text in lxml format 
     soup = BeautifulSoup(html_text, "lxml")
 
-    # gets all text content from page
-    content = soup.find('div', class_ ='mw-parser-output')
+    try: 
+        # gets all text content from page
+        content = soup.find('div', class_ ='mw-parser-output')
 
-    # gets all top level 'p' tags, which holds all the text
-    # paragraphs = content.find_all('p', recursive=False)
-    paragraphs = content.find_all('p')
+        # gets all top level 'p' tags, which holds all the text
+        # paragraphs = content.find_all('p', recursive=False)
+        paragraphs = content.find_all('p')
 
-    # used to help remove brackets and their contents (regex)
-    pattern = r'\[[^\]]*\]'
-    for i in range(len(paragraphs)): 
-        # removes HTML tags
-        paragraphs[i] = paragraphs[i].get_text()
-        # removes brackets and their contents 
-        paragraphs[i] = re.sub(pattern, '', paragraphs[i])
-        
-        # remove newline char
-        # paragraphs[i] = paragraphs[i].rstrip("\n")
-    # remove any empty lines
-    while("" in paragraphs):
-        paragraphs.remove("")
+        # used to help remove brackets and their contents (regex)
+        pattern = r'\[[^\]]*\]'
+        for i in range(len(paragraphs)): 
+            # removes HTML tags
+            paragraphs[i] = paragraphs[i].get_text()
+            # removes brackets and their contents 
+            paragraphs[i] = re.sub(pattern, '', paragraphs[i])
+            
+        # remove any empty lines
+        while("" in paragraphs):
+            paragraphs.remove("")
 
-    # main_text = f"{paragraphs}"
-    # main_text = ""
-    # for paragraph in paragraphs: 
-    #     main_text = main_text + " " + paragraph
+        return paragraphs
 
-    # return main_text
-    return paragraphs
+    # no text can be found from paragraphs
+    except: 
+        return []
 
 @app.get('/citations/{lang}/{tag}')
 # gets the citations in a citaitons array obj
@@ -253,68 +254,71 @@ def get_photos(lang: str, tag: str):
     
     # format the text in lxml format 
     soup = BeautifulSoup(html_text, "lxml")
-    images_array = []
 
-    # create empty array of image obj
-    for _ in soup.find_all('a', class_="image"): 
-        empty_img_obj = my_image("", "", 0)
-        images_array.append(empty_img_obj)
+    try: 
+        images_array = []
+        # create empty array of image obj
+        for _ in soup.find_all('a', class_="image"): 
+            empty_img_obj = my_image("", "", 0)
+            images_array.append(empty_img_obj)
 
-    # get infobox image first(if it exists)
-    if(soup.find_all("td", class_="infobox-image")): 
-        # text caption 
-        info_caption = soup.find("td", class_="infobox-image").get_text()
+        # get infobox image first(if it exists)
+        if(soup.find_all("td", class_="infobox-image")): 
+            # text caption 
+            info_caption = soup.find("td", class_="infobox-image").get_text()
 
-        # find the image src
-        info_wrapper = soup.find_all("td", class_="infobox-image")
-        for a in info_wrapper: 
-            for k in a.find_all('img', src=True): 
-                # add the https in front 
-                img_src = k['src']
+            # find the image src
+            info_wrapper = soup.find_all("td", class_="infobox-image")
+            for a in info_wrapper: 
+                for k in a.find_all('img', src=True): 
+                    # add the https in front 
+                    img_src = k['src']
+                    img_src = "https:" + img_src
+            
+            # populate tthe infobox
+            images_array[0].src = img_src
+            images_array[0].caption = info_caption
+            images_array[0].id = 1
+
+        image_text_idx = 0
+        image_src_idx = 0
+        # if there is a title image, populate 2nd array in list
+        if(images_array[0].id == 1): 
+            image_text_idx = 1
+            image_src_idx = 1
+
+        # find and populate the image caption 
+        other_images = soup.find_all("div", class_="thumb")
+        for image in other_images: 
+            image_caption = image.get_text()
+            images_array[image_text_idx].caption = image_caption
+            image_text_idx += 1
+
+        # find and populate the image source 
+        image_wrapper = soup.find_all("div", class_="thumbinner")
+        for wrapper in image_wrapper: 
+            for tag in wrapper.find_all('img', class_="thumbimage", src=True):
+                img_src = tag['src']
                 img_src = "https:" + img_src
-        
-        # populate tthe infobox
-        images_array[0].src = img_src
-        images_array[0].caption = info_caption
-        images_array[0].id = 1
+                images_array[image_src_idx].src = img_src
+                image_src_idx += 1
 
-    image_text_idx = 0
-    image_src_idx = 0
-    # if there is a title image, populate 2nd array in list
-    if(images_array[0].id == 1): 
-        image_text_idx = 1
-        image_src_idx = 1
+        final_images_array = []
 
-    # find and populate the image caption 
-    other_images = soup.find_all("div", class_="thumb")
-    for image in other_images: 
-        image_caption = image.get_text()
-        images_array[image_text_idx].caption = image_caption
-        image_text_idx += 1
+        # remove image instances with no source
+        for finalImage in images_array: 
+            if len(finalImage.src) == 0:
+                continue
+            final_images_array.append(finalImage)
 
-    # find and populate the image source 
-    image_wrapper = soup.find_all("div", class_="thumbinner")
-    for wrapper in image_wrapper: 
-        for tag in wrapper.find_all('img', class_="thumbimage", src=True):
-            img_src = tag['src']
-            img_src = "https:" + img_src
-            images_array[image_src_idx].src = img_src
-            image_src_idx += 1
+        # for finalImage in final_images_array: 
+        #     print(f"Image source: {finalImage.src}")
+        #     print(f"Image caption: {finalImage.caption}")
+        #     print(f"Image id: {finalImage.id}")
 
-    final_images_array = []
-
-    # remove image instances with no source
-    for finalImage in images_array: 
-        if len(finalImage.src) == 0:
-            continue
-        final_images_array.append(finalImage)
-
-    # for finalImage in final_images_array: 
-    #     print(f"Image source: {finalImage.src}")
-    #     print(f"Image caption: {finalImage.caption}")
-    #     print(f"Image id: {finalImage.id}")
-
-    return final_images_array
+        return final_images_array
+    except: 
+        return []
 
 @app.get('/categories/{lang}/{tag}')
 # gets the related categories
@@ -344,28 +348,32 @@ def get_categories(lang: str, tag: str):
     
     # format the text in lxml format 
     soup = BeautifulSoup(html_text, "lxml")
-    categories_wrapper = soup.find("div", class_="mw-normal-catlinks")
-    categories_array = []
-    # empty category array 
-    for category in categories_wrapper.find_all('li'): 
-        empty_category_obj= my_category("", "")
-        categories_array.append(empty_category_obj)
+    try: 
+        categories_wrapper = soup.find("div", class_="mw-normal-catlinks")
+        categories_array = []
+        # empty category array 
+        for category in categories_wrapper.find_all('li'): 
+            empty_category_obj= my_category("", "")
+            categories_array.append(empty_category_obj)
 
-    # get title for each category
-    text_idx = 0
-    for category in categories_wrapper.find_all('li'):
-        categories_array[text_idx].text = category.get_text()
-        text_idx += 1
-    
-    # get link for each category 
-    link_idx = 0
-    for single_category in categories_wrapper.find_all('ul'): 
-        for tag in single_category.find_all('a'): 
-            category_link = tag['href']
-            category_link = "https://en.wikipedia.org/" + category_link
-            categories_array[link_idx].link = category_link
-            link_idx+=1    
-    return categories_array
+        # get title for each category
+        text_idx = 0
+        for category in categories_wrapper.find_all('li'):
+            categories_array[text_idx].text = category.get_text()
+            text_idx += 1
+        
+        # get link for each category 
+        link_idx = 0
+        for single_category in categories_wrapper.find_all('ul'): 
+            for tag in single_category.find_all('a'): 
+                category_link = tag['href']
+                category_link = "https://en.wikipedia.org/" + category_link
+                categories_array[link_idx].link = category_link
+                link_idx+=1    
+        return categories_array
+    except: 
+        empty_category = my_category("", "No categories data")
+        return  [empty_category]
 
 # url = 'https://en.wikipedia.org/wiki/Lexus_F'
 # url = 'https://en.wikipedia.org/wiki/WWE'
